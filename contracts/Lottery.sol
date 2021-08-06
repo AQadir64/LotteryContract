161775cdf0c17f1274aaa27ff42681f15e1b62ed
AQadir64 <a.qadir67@outlook.com>
2021-08-06 16:09:46 +0500

    update : lottery contract to ethereum version >7

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,37 @@
-pragma solidity ^0.4.17;
+
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery {
+    
     address public manager;
     address[] public players;
     
-    function Lottery() public {
+    constructor() {
         manager = msg.sender;
     }
-    
     function enter() public payable {
-        require(msg.value > .01 ether);
+        require(msg.value > 0.1 ether);
         players.push(msg.sender);
+        
     }
     
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint)
+    {
+       return uint(keccak256(abi.encode(block.difficulty,block.timestamp,players)));
     }
     
-    function pickWinner() public restricted {
+    function pickPlayer() public restricted{
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
+        payable(players[index]).transfer(address(this).balance);
         players = new address[](0);
     }
     
-    modifier restricted() {
+    modifier restricted(){
         require(msg.sender == manager);
         _;
     }
     
-    function getPlayers() public view returns (address[]) {
+    function getPlayers() public view returns (address[] memory){
         return players;
     }
-}   
+}
